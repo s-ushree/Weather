bb514e74165ab6c461c9635058858bcf9693f78e
Sushree Pattanaik <217337012+s-ushree@users.noreply.github.com>
2026-06-24 17:26:28 +0530

    Fix HashRouter basename for GitHub Pages deployment

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,7 +9,7 @@ import 'react-toastify/dist/ReactToastify.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <HashRouter basename={import.meta.env.VITE_ROUTER_BASE || '/'}>
       <App />
       <ToastContainer position="top-right" autoClose={3000} />
     </HashRouter>
